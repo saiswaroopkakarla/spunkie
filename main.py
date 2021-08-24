b1601b05859dc806552c701a6a8b31d036c9692e
saiswaroopkakarla <89448849+saiswaroopkakarla@users.noreply.github.com>
2021-08-24 14:07:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,13 +12,12 @@ updater=Updater("1976152623:AAEWwWiH0klhAJdnUDFiP5gXiUNNc4ahTbk",use_context=Tru
 dispatcher=updater.dispatcher
 
 def doTheFlames(update:Update,context:CallbackContext):
-    bot.send_message("okok")
+    bot.send_message(
+    chat_id=update.effective_chat.id,
+    text="bot is working")
 
 start_value=CommandHandler("flames",doTheFlames)
-
-
-# In[ ]:
-
-
+dispatcher.add_handler(start_value)
+updater.start_polling()
 
 
